51b916daef8434c6cdc96b6e91092de8d69fb399
alin_g1000 <alin_g1000@yahoo.com>
2019-01-06 19:52:44 +0200

    simple shopping list bug fixed

--- Simple_Shopping_List/main.js
+++ Simple_Shopping_List/main.js
@@ -1,18 +1,20 @@
 
-
+var id_button= 3;
 			
 var list= document.querySelector('ul')
 
-var obj = document.querySelector('input:nth-child(2)')
+  var obj = document.querySelector('input:nth-child(2)')
 
 var inputbox = document.querySelector('input:nth-child(1)')
 
 var itemArray=Array.from(document.getElementsByClassName('itemButton'))
 
 
-obj.addEventListener('click',function(){
-
+obj.addEventListener('click', function(){
+  
     var x=document.createElement('li')
+    x.id=`${id_button}`
+    id_button++
 
     var sp1=document.createElement('span')
     sp1.innerText=inputbox.value
@@ -40,104 +42,19 @@ obj.addEventListener('click',function(){
     
 itemArray.forEach(function(element) {
 
-    element.addEventListener('click',function(){
-  
-       var index = itemArray.findIndex(x=>
-  
-          x.parentElement.parentElement.firstElementChild.innerText==element.parentElement.parentElement.firstElementChild.innerText)
-  
-      itemArray[index].parentElement.parentElement.remove()
-    })
-  });
- 
-})
-
-itemArray.forEach(function(element) {
-
   element.addEventListener('click',function(){
 
      var index = itemArray.findIndex(x=>
-
-        x.parentElement.parentElement.firstElementChild.innerText==element.parentElement.parentElement.firstElementChild.innerText)
+          //  alert(element.parentElement.parentElement.id)
+        x.parentElement.parentElement.id==element.parentElement.parentElement.id
+      )
 
     itemArray[index].parentElement.parentElement.remove()
   })
 });
-
-
-
-
-
-
-
-
-
-
-
-Javascript
-
-var list= document.querySelector('ul')
-
-var obj = document.querySelector('input:nth-child(2)')
-
-var inputbox = document.querySelector('input:nth-child(1)')
-
-var itemArray=Array.from(document.getElementsByClassName('itemButton'))
-
-
-obj.addEventListener('click',function(){
-
-    var x=document.createElement('li')
-
-    var sp1=document.createElement('span')
-    sp1.innerText=inputbox.value
-    inputbox.value=''
-
-    var sp2=document.createElement('span')
-    
-    var button=document.createElement('input')
-
-    button.type='submit'
-    button.value='Delete'
-    button.className='itemButton'
-    
-    sp2.appendChild(button)
-
-    x.appendChild(sp1)
-
-    x.appendChild(sp2)
-
-    
-    list.appendChild(x)
-
-    itemArray=Array.from(document.getElementsByClassName('itemButton'))
-
-    
-itemArray.forEach(function(element) {
-
-    element.addEventListener('click',function(){
-  
-       var index = itemArray.findIndex(x=>
-  
-          x.parentElement.parentElement.firstElementChild.innerText==element.parentElement.parentElement.firstElementChild.innerText)
-  
-      itemArray[index].parentElement.parentElement.remove()
-    })
-  });
- 
 })
 
-itemArray.forEach(function(element) {
 
-  element.addEventListener('click',function(){
-
-     var index = itemArray.findIndex(x=>
-
-        x.parentElement.parentElement.firstElementChild.innerText==element.parentElement.parentElement.firstElementChild.innerText)
-
-    itemArray[index].parentElement.parentElement.remove()
-  })
-});
 
 
 

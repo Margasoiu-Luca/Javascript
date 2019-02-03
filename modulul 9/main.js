2b669aac8f156b94f0cbf97610691c7acfdc88fc
Student <student@stepit.loc>
2019-02-03 21:00:55 +0200

    version 0.9

--- modulul 9/main.js
+++ modulul 9/main.js
@@ -44,6 +44,7 @@ function generateCat() {
                 generateCat()
             }
             document.querySelector("img:nth-child(1)").src = data[0].url
+            document.querySelector("img:nth-child(1)").title = data[0].breeds[0].name
         })
 
 }
@@ -72,34 +73,44 @@ document.getElementById('secondary_search').addEventListener('click', (ev) => {
     x.forEach(el => {
         fetch('https://api.thecatapi.com/v1/images/search?category_ids=' + document.querySelector('.active').firstElementChild.id, login)
             .then(res => res.json())
-            .then(data => el.src = data[0].url)
+            .then(data => {el.src = data[0].url
+                            el.title=""})
     })
 })
 
 //event listener pentru clear
 document.getElementsByClassName('btn-secondary')[8].addEventListener('click', () => {
     var x = document.querySelectorAll('.poza_secundara')
-    x.forEach(el => el.src = "http://vignette1.wikia.nocookie.net/dynastywarriors/images/6/60/Link_-_HW.png/revision/latest?cb=20140526003852")
+    x.forEach(el => {el.src = "http://vignette1.wikia.nocookie.net/dynastywarriors/images/6/60/Link_-_HW.png/revision/latest?cb=20140526003852"
+                    el.title="Placeholder"})
 })
 
 
-// var y = Array.from(document.querySelectorAll('.btn-secondary'))
-// y.forEach(el => el.addEventListener('click', checkBtnSecondary))
+//aici se face selectia la intamplare a imaginilor care vor aparea
+function searchCatFunction(x,arrLength){
+    for (let i = 0; i < 4; i++) {
+        //pisica care se alege la intamplare 
+        var rand_cat = Math.floor(Math.random() * arrLength)
+        //poza care se alege din cele 6 generate
+        var rand2_index= Math.floor(Math.random() * 6)
+        var reference=document.getElementById(`img${i+1}`)
+        reference.src=x[rand_cat][rand2_index].url
+        reference.title=x[rand_cat][rand2_index].breeds[0].name
+        // console.dir(x[rand_cat][rand2_index])
+    }}
 
-// function checkBtnSecondary(ev) {
-//     console.dir(ev.target.classList)
-// }
 
 //netflix-like search event listener pe input
 inputBox.addEventListener('keyup', (ev) => {
-    if (ev.target.value >= 2) {
+    if (ev.target.value.length >= 2) {
         //aceasta linie de cod va filtra arrayul de pisici pentru a crea un nou array cu rasele de pisisci care contin numele
-        var filteredBreeds = total_breeds.filter(item => item.name.toLowerCase().indexOf(inputBox.value.toLowerCase()) !== -1) 
+        var filteredBreeds = total_breeds.filter(item => item.name.toLowerCase().indexOf(inputBox.value.toLowerCase()) !== -1)
         //dupa acea se va face un map care va schimba arrayul de pisici intr-un aray de promise
-        filteredBreeds.map(item => fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${item.id}`, login).then(res => res.json()))
+        var promiseList = filteredBreeds.map(item => fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${item.id}&limit=6`, login).then(res => res.json()))
 
-        Promise.all(filteredBreeds)
-            .then(data => console.dir(data))
+        Promise.all(promiseList)
+            .then(data => searchCatFunction(data,promiseList.length))
+
     }
 
 })
